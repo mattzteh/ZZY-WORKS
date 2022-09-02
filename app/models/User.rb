2f637f08ad2fa3bd232733a7c6c0ff2f9c408d03
mattzteh <mattzteh@gmail.com>
2022-09-02 15:14:22 -0700

    finish backend auth

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,51 +1,36 @@
 class User < ApplicationRecord
-
-    before_validation :ensure_session_token
-
-    validates :first_name, :last_name, presence: true
-    validates :email, uniqueness: true, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    
+    has_secure_password
+    
+    validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :session_token, presence: true, uniqueness: true
     validates :password, length: { in: 6..255 }, allow_nil: true
-
-    attr_reader :password
-
-    def self.find_by_credentials(email, password)
-        user = User.find_by(email:)
-        if user && user.is_password?(password)
-            user
-        else
-            nil
-        end
-    end
-
-    def password=(password)
-        @password = password
-        self.password_digest = BCrypt::Password.create(password)
-    end
-
-    def is_password?(password)
-        bcrypt_obj = BCrypt::Password.new(self.password_digest)
-        bcrypt_obj.is_password?(password)
+    before_validation :ensure_session_token
+    
+    def self.find_by_credentials(credential, password)
+        field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :username
+        user = User.find_by(field => credential)
+        user&.authenticate(password)
     end
 
     def reset_session_token!
-        self.session_token = generate_session_token
-        self.save
+        self.update!(session_token: generate_unique_session_token)
         self.session_token
     end
 
     private
 
-    def ensure_session_token
-        self.session_token ||= generate_session_token
-    end
-
-    def generate_session_token
-        token = SecureRandom::urlsafe_base64
-        while User.exists?(session_token: token)
-            token = SecureRandom::urlsafe_base64
+    def generate_unique_session_token
+        loop do
+            token = SecureRandom.base64
+            break token unless User.exists?(session_token: token)
         end
-        token
     end
 
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_210723) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["session_token"], name: "index_users_on_session_token"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end

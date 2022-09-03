38f6a9a93a8b9b5e27c7b4232feba7364407bcec
mattzteh <mattzteh@gmail.com>
2022-09-02 17:10:08 -0700

    start heroku deployment

---
+++ frontend/src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider } from './context/Modal';
+import './index.css';
+import App from './App';
+import configureStore from './store';
+import csrfFetch from "./store/csrf";
+import * as sessionActions from './store/session';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
+}
+
+// const Carrot = () => (
+//   <div style={{ color: "orange", fontSize: "100px" }}>
+//     <i className="fa-solid fa-carrot"></i>
+//   </div>
+// );
+
+function Root() {
+  return (
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          {/* <Carrot /> */}
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  );
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}

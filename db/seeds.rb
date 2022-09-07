fde6ea9bd771ad2ff966116d6e5b8cdf58afbc25
mattzteh <mattzteh@gmail.com>
2022-09-07 12:23:52 -0700

    continue seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ ApplicationRecord.transaction do
 
 # OFFICIAL SEED DATA------------------------------------------------------------
 duck_switches = Product.create(
-  name: 'Duck Switches',
+  name: 'DUCK Switches',
   price: '$12',
   description: 'In development for over the past six months, we are excited to 
   introduce our latest innovation - the DUCK switch. Working closely with our 
@@ -50,12 +50,13 @@ duck_switches = Product.create(
   
 duck_switches.photos.attach(
   io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/M60_splash.webp"), 
-  filename: "M60_splash.webp")
+  filename: "M60_splash.webp"
+)
 
 #-------------------------------------------------------------------------------
   
 grid_set_a = Product.create(
-  name: 'Grid Set A Keycaps',
+  name: 'GRID Set A Keycaps',
   price: '$42',
   description: 'Injection moulded, low-profile keycaps produced by RAMA WORKS®. 
   Available in single colour sets (62 caps) or a mixed set (108 caps)'
@@ -79,7 +80,7 @@ grid_set_a.photos.attach(
 #-------------------------------------------------------------------------------
 
 grid_set_b = Product.create(
-  name: 'Grid Set B Keycaps',
+  name: 'GRID Set B Keycaps',
   price: '$42',
   description: 'Injection moulded, low-profile keycaps produced by RAMA WORKS®. 
   Available in single colour sets (62 caps) or a mixed set (100 caps)'
@@ -103,7 +104,7 @@ grid_set_b.photos.attach(
 #-------------------------------------------------------------------------------
 
 grid_set_c = Product.create(
-  name: 'Grid Set C Keycaps',
+  name: 'GRID Set C Keycaps',
   price: '$42',
   description: 'Injection moulded, low-profile keycaps produced by RAMA WORKS®. 
   Available in single colour sets (62 caps) or a mixed set of select 
@@ -127,8 +128,220 @@ grid_set_c.photos.attach(
 
 #-------------------------------------------------------------------------------
 
+kara_backplate = Product.create(
+  name: 'KARA Backplate (Extra)',
+  price: '$26',
+  description: 'NOTE: MATCHING BACKPLATE IS ALREADY INCLUDED WITH KARA. NOT COMPATIBLE WITH M60-A.'
+)
 
+kara_backplate.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KARA_BACKPLATE_PHOTO_1.webp"),
+  filename: "KARA_BACKPLATE_PHOTO_1.webp"
+)
+
+kara_backplate.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KARA_BACKPLATE_PHOTO_2.webp"),
+  filename: "KARA_BACKPLATE_PHOTO_2.webp"
+)
+
+kara_backplate.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KARA_BACKPLATE_PHOTO_3.webp"),
+  filename: "KARA_BACKPLATE_PHOTO_3.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+kara_plate = Product.create(
+  name: 'KARA Plate (Extra)',
+  price: '$16',
+  description: 'NOTE: THE ICED PLATE IS ALREADY INCLUDED WITH KARA. NOT COMPATIBLE WITH M60-A. 
+  KARA SUB-ASSEMBLY REQUIRES ALL 3 PARTS (PCB, MUTE, PLATE)
+
+  Product Name:
+  RAMA WORKS® KARA PLATE
+
+  Part Number:
+  RW-KARA-A-PLATE
+
+  Material:
+  Polycarbonate (NOCT, SOYA, ICED, AZUR, HAZE)
+
+  Aluminium (MIST)'
+)
+
+kara_plate.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KARA_PLATE.webp"),
+  filename: "KARA_PLATE.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+keycap = Product.create(
+  name: 'WAVE SEQ2 Br Matte',
+  price: '$56',
+  description: 'IN-STOCK - LIMITED RELEASE
+
+
+  Product Name:
+
+  RAMA WORKS® Br WAVE SEQ 2
+
+  Part Number:
+
+  RW-KC-Br-WAVE-SEQ2-01
+
+  -------------------------------------------------------------------------------
+
+  Product Description:
+
+  The return of the LIQUID SERIES in the WAVE SEQ2 tactile keycap series by RAMA WORKS®.
+
+  -------------------------------------------------------------------------------
+
+  Product Color:
+  GOLD / MOON
+
+  Material:
+  Brass
+
+  Series:
+  WAVE SEQ2
+
+  Stem Type:
+  Cherry MX
+
+  Profile:
+  Cherry Profile - Escape Row (R1)'
+)
+
+keycap.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KEYCAP_PHOTO_1.webp"),
+  filename: "KEYCAP_PHOTO_1.webp"
+)
+
+keycap.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KEYCAP_PHOTO_2.webp"),
+  filename: "KEYCAP_PHOTO_2.webp"
+)
   
+keycap.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/KEYCAP_PHOTO_3.webp"),
+  filename: "KEYCAP_PHOTO_3.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+m65_starter_kit = Product.create(
+  name: 'M65-B Starter Kit',
+  price: '$38',
+  description: 'Everything you need sans keycaps to make this a complete kit 
+  with Aliaz 60g.'
+)
+
+m65_starter_kit.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/M65_B_STARTER_KIT.webp"),
+  filename: "M65_B_STARTER_KIT.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+m65_mute = Product.create(
+  name: 'M65-B Mute (Extra)',
+  price: '$38',
+  description: 'Extra MUTE mount for the M65-B, non-conductive silicone. 
+  Need a spare? Making another plate? Moulded out of clear silicone to match 
+  perfectly with the ICED variant.'
+)
+
+m65_mute.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/M65_MUTE_PHOTO_1.webp"),
+  filename: "M65_MUTE_PHOTO_1.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+m65_pcb = Product.create(
+  name: 'M65-B PCB (Extra)',
+  price: '$88',
+  description: 'In an effort to provide the best end-user experience available, 
+  this M65-B PCB requires no soldering to construct with any Cherry 
+  MX-compatible switches. The M65-B offers a USB-C connection attached to the 
+  USB Extension PCB. The board includes in-switch RGB LEDs under the PCB, which 
+  require no installation. PCB design by Wilba.
+  
+  The M65-Bx variant is a solderable version without hot-swap sockets for those 
+  who are discerning with their switch choice.'
+)
+
+m65_pcb.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/M65_PCB_PHOTO_1.webp"),
+  filename: "M65_PCB_PHOTO_1.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+m65_plate = Product.create(
+  name: 'M65-B Plate (Extra)',
+  price: '$52',
+  description: 'Product Description:
+  Extra plate for the M65-B, Mirror-finished PVD Coated Brass, Anodised Aluminium or Frosted Polycarbonate.
+
+  -------------------------------------------------------------------------------
+
+  Product Color Options:
+  Brass: KURO (Black), MOON (Grey), MIST (Silver), ROSE
+  Aluminium: KURO
+  Polycarbonate: ICED
+
+  Finish:
+  Brass: Hand-polished Mirror finished PVD Coated Brass
+  Aluminium: Beadblasted Anodised Aluminium
+  Polycarbonate: Frosted
+
+  -------------------------------------------------------------------------------
+
+  Contents:
+
+  1 x Plate (Brass OR Aluminium)
+
+  6 x RW Black M2x3 T10 Fixtures
+
+  6 x RW Black M2x3 CSK T6 Fixtures
+
+  6 x RW Black M2x3.5 Spacer
+
+  1 x Various Packaging
+
+  -------------------------------------------------------------------------------'
+)
+
+m65_plate.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/M65_PLATE_PHOTO_1.webp"),
+  filename: "M65_PLATE_PHOTO_1.webp"
+)
+
+#-------------------------------------------------------------------------------
+
+deskmat = Product.create(
+  name: 'MAT',
+  price: '$24',
+  description: 'DESCRIPTION: 
+  
+  We are excited to introduce our new MATS, available exclusively through RAMA WORKS®. This improved desk mat comes in four different colourways, perfect for matching any keyboard and desk setup combination.
+
+  MATS is made out of an ultra-smooth, soft & sound absorbent material, measuring 300mm (W) x 720mm (L)  and a perfect thickness of 4mm (H), which is the same size as our much loved CANVAS XL.
+
+  Machine Washable (wash on cold/gentle - air dry)
+  Rubber foam base to ensure stability
+  Stitched edge around the border for durability and prevent frayed edges
+  Cloth Top - 100% polyester, which is soft to touch and perfect for your mouse to glide on'
+)
+
+deskmat.photos.attach(
+  io: URI.open("https://zzyworks-dev.s3.us-west-1.amazonaws.com/MATS_PHOTO_1.webp"),
+  filename: "MATS_PHOTO_1.webp"
+)
+
 
 
 puts "Done!"

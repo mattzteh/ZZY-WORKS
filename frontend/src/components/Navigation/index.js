42b9ff0f62638ea4d59a4e6507fecde4fd2b971c
mattzteh <mattzteh@gmail.com>
2022-09-13 18:47:15 -0700

    restyle nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -39,31 +39,28 @@ function Navigation() {
   <div className={navToggle()}>
     <ul>
       <li className='navitems'>
-        <div className='nav-name'>
-          <NavLink exact to="/" style={{ textDecoration: 'none' }}><h1 className='name'>ZZY WORKS</h1></NavLink>
-          <NavLink exact to="/" style={{ textDecoration: 'none' }}><div className='store'>Store</div></NavLink>
-          <NavLink exact to="/catalog" style={{ textDecoration: 'none' }}><div className='catalog'>Catalog</div></NavLink>
-          <NavLink exact to="/" style={{ textDecoration: 'none' }}><div className='studio'>Studio</div></NavLink>
-          <NavLink exact to="/" style={{ textDecoration: 'none' }}><div className='updates'>Updates</div></NavLink>
-        </div>
 
-        <div className='nav-links'>
-       
-          <div className='icons'>
-            {sessionLinks}
-            <NavLink to="/" className="search"><i className="fa-solid fa-magnifying-glass"></i></NavLink>
-
-            <div className='cart-collapse'>
-              <button className="cart" onClick={() => setCartMenu(true)}>
-                <i className="fa-solid fa-cart-shopping"></i>
-              </button>
-              {cartMenu && <CartIndexPage closeCartMenu={setCartMenu}/>}
-            </div>
-
+        <div className='nav-name'><NavLink exact to="/" style={{ textDecoration: 'none' }}><h1 className='name'>ZZY WORKS</h1></NavLink></div>
+          <div className='nav-links'>
+            <NavLink exact to="/" style={{ textDecoration: 'none' }}><div className='store'>Store</div></NavLink>
+            <NavLink exact to="/catalog" style={{ textDecoration: 'none' }}><div className='catalog'>Catalog</div></NavLink>
+            <NavLink exact to="/" style={{ textDecoration: 'none' }}><div className='studio'>Studio</div></NavLink>
+            <NavLink exact to="/" style={{ textDecoration: 'none' }}><div className='updates'>Updates</div></NavLink>
           </div>
-        </div>
       </li>
     </ul>
+   
+    <div className='icons'>
+      {sessionLinks}
+      <NavLink to="/" className="search"><i className="fa-solid fa-magnifying-glass"></i></NavLink>
+
+      <div className='cart-collapse'>
+        <button className="cart" onClick={() => setCartMenu(true)}>
+          <i className="fa-solid fa-cart-shopping"></i>
+        </button>
+        {cartMenu && <CartIndexPage closeCartMenu={setCartMenu}/>}
+      </div>
+    </div>
   </div>
   </>
   );

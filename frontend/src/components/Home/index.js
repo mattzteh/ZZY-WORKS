76e7071f8a8149ba75f0ccc1735b3e65b8699b73
mattzteh <mattzteh@gmail.com>
2022-09-14 18:03:29 -0700

    recropped switch splash image

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
-import u80Splash from './u80Splash.webp'
-import thermalSplash from './thermalSplash.jpeg'
-import keycapSplash from './keycapSplash.jpeg'
+import u80Splash from './assets/u80Splash.webp'
+import thermalSplash from './assets/thermalSplash.jpeg'
+import keycapSplash from './assets/keycapSplash.jpeg'
+import duckSplash from './assets/duckSplash.jpeg'
 import './Home.css';
 
 const Home = () => {
@@ -21,10 +22,19 @@ const Home = () => {
             <img src={thermalSplash}></img>
         </div>
 
+        <div className="duck">
+            <div className='splash-desc'>
+                <h1>DUCK</h1>
+                <h2>LINEAR SWITCHES</h2>
+                <Link to="/products/2"><button>BUY</button></Link>
+            </div>
+            <img src={duckSplash}></img>
+        </div>
+
         <div className="grid-keycaps">
             <div className='splash-desc'>
                 <h1>GRID</h1>
-                <h2>MX Chiclets / Keycaps</h2>
+                <h2>MX CHICLETS / KEYCAPS</h2>
                 <Link to="/products/3"><button>BUY</button></Link>
             </div>
             <img src={keycapSplash}></img>
